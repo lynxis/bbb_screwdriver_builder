f5ea32cd5bb601ce27315a015d2b39bd6489dc1f
Alexander Couzens <lynxis@fe80.eu>
2016-06-14 16:37:19 -0700

    config.mk: switch back to lede source

--- config.mk
+++ config.mk
@@ -2,6 +2,6 @@
 SHELL:=$(shell which bash)
 TARGET=omap_generic
 PACKAGES_LIST_DEFAULT=default
-LEDE_SRC=https://git.lede-project.org/lede/lynxis/staging/omap.git
-LEDE_COMMIT=9f142f4721e77db3a014ba17b8326d15a20a4264
+LEDE_SRC=https://git.lede-project.org/source.git
+LEDE_COMMIT=9e45f9d63c797baab2b3bd2574990ee94d3f80cb
 MAKE_ARGS=
