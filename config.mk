0735d6f98b2863fe7da6f4f4ade27198790a6bcd
Alexander Couzens <lynxis@fe80.eu>
2016-06-14 22:58:42 -0700

    config.mk: update lede revision

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=omap_generic
 PACKAGES_LIST_DEFAULT=default
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=9e45f9d63c797baab2b3bd2574990ee94d3f80cb
+LEDE_COMMIT=719fd09d91682c00c18bb29c7785638a1a83b622
 MAKE_ARGS=
